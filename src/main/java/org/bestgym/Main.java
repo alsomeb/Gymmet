c58f92d998ce57317d0de822da165a39d18301de
alsomeb <alsomeb@gmail.com>
2022-10-19 15:16:25 +0200

    updated comments

--- src/main/java/org/bestgym/Main.java
+++ src/main/java/org/bestgym/Main.java
@@ -3,6 +3,6 @@ package org.bestgym;
 
 public class Main {
     public static void main(String[] args) {
-        new GymApp(false); // testMode avaktiverat pga vi är i "produktion"
+        new GymApp(false); // testMode avaktiverat pga vi kör "produktion" appen
     }
 }
